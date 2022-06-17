1db4b307a6e6c69a4af42662a8da84a5cf6a8d3b
linebot202206 <107710883+linebot202206@users.noreply.github.com>
2022-06-18 06:10:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
-<?php
-  CREATE DATABASE '123';
-?>
+<html>
+   <head>
+      <title>Connecting MySQL Server</title>
+   </head>
+   <body>
+      <?php
+         $dbhost = 'remotemysql.com:3306';
+         $dbuser = '9b3cxQX9UY';
+         $dbpass = '3EdzRNODN8';
+         $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+         
+         if(! $conn ) {
+            die('Could not connect: ' . mysqli_error());
+         }
+         echo 'Connected successfully';
+         mysqli_close($conn);
+      ?>
+   </body>
+</html>

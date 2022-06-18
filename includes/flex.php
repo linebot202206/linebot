3e2c80680114e8f4b1ce32fecabaa64f47190070
Sylvia <linebot202206@gmail.com>
2022-06-19 02:36:39 +0800

    'try'

--- includes/flex.php
+++ includes/flex.php
@@ -1,6 +1,6 @@
 <?php
 global $client, $message, $event;
-if (strtolower($message['text']) == "flex" || $config['type'] == "flex") {
+if (strtolower($message['text']) == "flex") {
     /* 注意，Flex Message Simulator 生成並轉換的陣列貼在這邊 */
     $name = $message['text'];
     $contentsArray = output($name);

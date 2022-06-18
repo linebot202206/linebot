b43624ce726a73e27281dcd7fa3d13e372a015c9
Sylvia <linebot202206@gmail.com>
2022-06-19 05:35:14 +0800

    'try'

--- liff/index.php
+++ liff/index.php
@@ -7,7 +7,8 @@
     <?php
     $urlData = urldecode($_SERVER['QUERY_STRING']);
     $url = urldecode(explode("=",explode("?",$urlData)[1])[1]);
-    header("Location: $url"); 
+    //header("Location: $url");
+    header("Location: https://www.google.com.tw/maps/place/%E6%A2%85%E5%B1%B1%E5%AF%8C%E9%87%8C%E5%B1%85/@23.552816,120.553139,17z/data=!3m1!4b1!4m5!3m4!1s0x346ec0f4f20d8a8b:0x34e2ec79b9cf71ca!8m2!3d23.5528016!4d120.5531011?hl=zh-TW&authuser");
     exit;
     ?>
   </body>

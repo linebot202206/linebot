8c6923680d0db99e9c447711674c6a0299b1f529
Sylvia <linebot202206@gmail.com>
2022-06-19 02:53:35 +0800

    'try'

--- includes/base.php
+++ includes/base.php
@@ -1,6 +1,6 @@
 <?php
 
-$sql = "SELECT * FROM `config` WHERE `name` = '".strtolower($message)."'";
+$sql = "SELECT * FROM `config` WHERE `name` = '".strtolower($message['text'])."'";
 $retval = mysqli_query( $conn, $sql );
 if($retval) {
   $row = mysqli_fetch_array($retval, MYSQLI_ASSOC);

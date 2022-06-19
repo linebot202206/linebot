419cb67fd6c6eb975e4feabf7c3bbb9711f00391
Sylvia <linebot202206@gmail.com>
2022-06-20 00:24:21 +0800

    'try'

---
+++ liff/tall - 複製.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+  </head>
+  <body>
+    <?php
+    $urlData = urldecode($_SERVER['QUERY_STRING']);
+    $url = urldecode(substr(explode("?",$urlData)[1], 4));
+    header("Location: $url");
+    exit;
+    ?>
+  </body>
+</html>

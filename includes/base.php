70e0c520f406309f33c3567827c6ef9c76455eba
Sylvia <linebot202206@gmail.com>
2022-06-19 17:25:05 +0800

    'try'

--- includes/base.php
+++ includes/base.php
@@ -1,10 +1,25 @@
 <?php
 
+/*
 $sql = "SELECT * FROM `config` WHERE `name` = '".strtolower($message['text'])."'";
 $retval = mysqli_query( $conn, $sql );
 if($retval) {
   $row = mysqli_fetch_array($retval, MYSQLI_ASSOC);
 	$config = $row;
+}
+*/
+
+$command = explode(" ",$message['text']);
+
+$sql = "SELECT * FROM `config`";
+$retval = mysqli_query( $conn, $sql );
+if($retval) {
+    while ($row = mysqli_fetch_array($retval, MYSQLI_ASSOC)) {
+        $data[$row['name']] = $row;
+    }
+
+    $type = $command[0];
+	$name = isset($command[1])?$command[1]:$command[0];
 }
 
 ?>

a27ab7d3b0ac853c127a2f6299d0a1a1b68427d9
Sylvia <linebot202206@gmail.com>
2022-06-20 02:06:01 +0800

    try

--- includes/text.php
+++ includes/text.php
@@ -27,7 +27,8 @@ if (strtolower($message['text']) == "text" || $message['text'] == "文字") {
         'messages' => array(
             array(
                 'type' => 'text', //訊息類型 (文字)
-                'text' => 'Hello, world!'.$a //回覆訊息
+                'text' => $event['source']['userId'] //回覆訊息
+                //'text' => 'Hello, world!'.$a //回覆訊息
             )
         )
     ));

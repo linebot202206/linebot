e55c7c1ee4c720f7a276a2285f868aa305670175
Sylvia <linebot202206@gmail.com>
2022-06-20 03:49:05 +0800

    try

--- LINEBotTiny.php
+++ LINEBotTiny.php
@@ -115,34 +115,6 @@ class LINEBotTiny
         }
     }
 
-    public function profile()
-    {
-        /*
-        Response = 
-        {
-            "userId": "U43b3b5392877af9ca69fb0ad7017e052",
-            "displayName": "欣慧",
-            "pictureUrl": "https://sprofile.line-scdn.net/0hd8HYjlEHOxdlIC_yMrlFaBVwOH1GUWIFQUMkc1d0bHUME3lDSEYkc1AiZi9eQnhAGU52c1ZyYnRpM0xxe3bHI2IQZSBcFn9DSEd29w",
-            "language": "zh-TW"
-        }
-        */
-        $header = array(
-            'Content-Type: application/json',
-            'Authorization: Bearer ' . $this->channelAccessToken,
-        );
-
-        $context = stream_context_create([
-            'http' => [
-                'method' => 'GET',
-                'header' => implode("\r\n", $header),
-            ],
-        ]);
-
-        $response = file_get_contents("https://api.line.me/v2/bot/profile/U43b3b5392877af9ca69fb0ad7017e052"., false, $context);
-        
-        return $response;
-    }
-
     private function sign($body)
     {
         $hash = hash_hmac('sha256', $body, $this->channelSecret, true);

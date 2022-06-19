7d224f044037833d6b7100568cbdcc452e1f0db7
Sylvia <linebot202206@gmail.com>
2022-06-20 03:26:50 +0800

    try

--- includes/text.php
+++ includes/text.php
@@ -22,14 +22,14 @@
 */
 global $client, $message, $event;
 if (strtolower($message['text']) == "text" || $message['text'] == "文字") {
-    $profile = json_decode($client-> profile(), true);
+    $profile = $client-> profile();
 
     $client->replyMessage(array(
         'replyToken' => $event['replyToken'],
         'messages' => array(
             array(
                 'type' => 'text', //訊息類型 (文字)
-                'text' => $event['source']['userId']."===".$profile['displayName'] //回覆訊息
+                'text' => $event['source']['userId']."===".$event['source']['source'] //回覆訊息
                 //'text' => 'Hello, world!'.$a //回覆訊息
             )
         )

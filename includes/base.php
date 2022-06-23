f08c82333fc7c81a3cbfec444e86b0459ea14205
Sylvia <linebot202206@gmail.com>
2022-06-23 23:08:12 +0800

    'try'

--- includes/base.php
+++ includes/base.php
@@ -8,18 +8,49 @@ if($retval) {
 	$config = $row;
 }
 */
+if (strpos( $message['text'], "#" ) === 0) {
+	$type = ($event['source']['type'] == "user")?1:2;
+	$id = ($type==1)?$event['source']['userId']:$event['source']['groupId'];
+	$sql = "SELECT * FROM `command` WHERE `cmd` = $message['text'] AND `type` = $type AND `id` = $id";
+	/*
+	$retval = mysqli_query( $conn, $sql );
+	if($retval) {
+	    $row = mysqli_fetch_array($retval, MYSQLI_ASSOC)
+	    $client->replyMessage(array(
+	        'replyToken' => $event['replyToken'],
+	        'messages' => array(
+	            array(
+	                'type' => 'text', //訊息類型 (文字)
+	                'text' => $row['text']
+	                //'text' => 'Hello, world!'.$profile['displayName'] //回覆訊息
+	            )
+	        )
+	    ));
+	}
+	*/
+	$client->replyMessage(array(
+        'replyToken' => $event['replyToken'],
+        'messages' => array(
+            array(
+                'type' => 'text', //訊息類型 (文字)
+                'text' => $sql
+                //'text' => 'Hello, world!'.$profile['displayName'] //回覆訊息
+            )
+        )
+    ));
+}else{
+	$command = explode(" ",$message['text']);
 
-$command = explode(" ",$message['text']);
+	$sql = "SELECT * FROM `config`";
+	$retval = mysqli_query( $conn, $sql );
+	if($retval) {
+	    while ($row = mysqli_fetch_array($retval, MYSQLI_ASSOC)) {
+	        $data[$row['name']] = $row;
+	    }
 
-$sql = "SELECT * FROM `config`";
-$retval = mysqli_query( $conn, $sql );
-if($retval) {
-    while ($row = mysqli_fetch_array($retval, MYSQLI_ASSOC)) {
-        $data[$row['name']] = $row;
-    }
-
-    $type = $data[$command[0]]['type'];
-	$name = isset($command[1])?$command[1]:$command[0];
+	    $type = $data[$command[0]]['type'];
+		$name = isset($command[1])?$command[1]:$command[0];
+	}
 }
 
 ?>

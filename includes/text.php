771740a8305007823e1f61fc40ee6d300ddc45ca
Sylvia <linebot202206@gmail.com>
2022-06-23 21:34:28 +0800

    'try'

--- includes/text.php
+++ includes/text.php
@@ -28,7 +28,7 @@ if (strtolower($message['text']) == "text" || $message['text'] == "文字") {
         'messages' => array(
             array(
                 'type' => 'text', //訊息類型 (文字)
-                'text' => substr($message['text'], 0, 1);
+                'text' => $message['text'];
                 //'text' => 'Hello, world!'.$profile['displayName'] //回覆訊息
             )
         )

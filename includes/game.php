4113a085ddcfa8ca15520580c2de92deefa89100
Sylvia <linebot202206@gmail.com>
2022-06-25 15:05:43 +0800

    'try'

---
+++ includes/game.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Copyright 2020 GoneTone
+ *
+ * Line Bot
+ * 範例 Example Bot (Text)
+ *
+ * 此範例 GitHub 專案：https://github.com/GoneToneStudio/line-example-bot-tiny-php
+ * 此範例教學文章：https://blog.reh.tw/archives/988
+ *
+ * 官方文檔：https://developers.line.biz/en/reference/messaging-api#text-message
+ */
+
+/*
+陣列輸出 Json
+==============================
+{
+    "type": "text",
+    "text": "Hello, world!"
+}
+==============================
+*/
+global $client, $message, $event;
+
+$game = rand ( 70 , 9999 );
+
+}
